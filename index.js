8091a73ba7735fc309eb85c1df995981c6802aad
mrjeoffrey <b.jeoffrey@gmail.com>
2022-02-16 23:17:17 -0800

    start index.js

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+	title,
+	description,
+	install,
+	usage,
+	credits,
+	license,
+	badges,
+	features,
+	contributor,
+	tests,
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.appendFile(`${fileName}.md`, data, 
+    (err) => err ? console.error(err) : console.log(`${filename}.md generated.`)
+}
 
 // TODO: Create a function to initialize app
 function init() {}
