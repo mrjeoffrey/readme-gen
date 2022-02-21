fc22b5a82039a1d84112f02b39156973a5940555
mrjeoffrey <b.jeoffrey@gmail.com>
2022-02-21 12:18:15 -0800

    broken. need to debug

--- index.js
+++ index.js
@@ -16,76 +16,6 @@ const questions = [
 	tests,
 ];
 
-const prompt = [
-	{
-		type: "input",
-		name: "name",
-		message: "What is your name?",
-	},
-	{
-		type: "input",
-		name: "location",
-		message: "Where are you from?",
-	},
-	{
-		type: "input",
-		name: "hobby",
-		message: "What is your favorite hobby?",
-	},
-	{
-		type: "input",
-		name: "food",
-		message: "What is your favorite food?",
-	},
-	{
-		type: "input",
-		name: "github",
-		message: "Enter your GitHub Username",
-	},
-	{
-		type: "input",
-		name: "linkedin",
-		message: "Enter your LinkedIn URL.",
-	},
-];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-	fs.appendFile(`${fileName}.md`, data, (err) =>
-		err ? console.error(err) : console.log(`${filename}.md generated.`)
-	);
-}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
-
-const generateHTML = ({ name, location, github, linkedin }) =>
-	`<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${name}</h1>
-    <p class="lead">I am from ${location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${github}</li>
-      <li class="list-group-item">LinkedIn: ${linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
-
 inquirer
 	.prompt([
 		{
@@ -119,8 +49,47 @@ inquirer
 			message: "Enter your LinkedIn URL.",
 		},
 	])
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+	fs.appendFile(`${fileName}.md`, data, (err) =>
+		err ? console.error(err) : console.log(`${filename}.md generated.`)
+	);
+}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+const generateMD = ({ name, location, github, linkedin }) =>
+	`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Hi! My name is ${name}</h1>
+    <p class="lead">I am from ${location}.</p>
+    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+    <ul class="list-group">
+      <li class="list-group-item">My GitHub username is ${github}</li>
+      <li class="list-group-item">LinkedIn: ${linkedin}</li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>`;
+
+
 	.then((answers) => {
-		const htmlPageContent = generateHTML(answers);
+		const htmlPageContent = generateMD(answers);
 
 		fs.writeFile("index.html", htmlPageContent, (err) =>
 			err ? console.log(err) : console.log("Successfully created index.html!")
