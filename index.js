0481cb83a8f6c881dbb0b0d11161b5b8d29ddd4b
mrjeoffrey <b.jeoffrey@gmail.com>
2022-02-21 23:47:23 -0800

    redoing questions

--- index.js
+++ index.js
@@ -1,61 +1,129 @@
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
+
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [
-	title,
-	description,
-	install,
-	usage,
-	credits,
-	license,
-	badges,
-	features,
-	contributor,
-	tests,
-];
+const userQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the title of your project?",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "Enter a description for your project:",
+            name: "description"
+        },
+        {
+            type: "input",
+            message: "Enter installation instructions for your project:",
+            name: "install"
+        },
+        {
+            type: "input",
+            message: "Enter usage information for your project:",
+            name: "usage"
+        },
+        {
+            type: "input",
+            message: "Who are the collaborators of this projects?",
+            name: "credits"
+        },
+        {
+            type: "input",
+            message: "Enter your GitHub Username:",
+            name: "github"
+        },
+        {
+            type: "input",
+            message: "Enter your email address:",
+            name: "email"
+        },
+        {
+            type: "list",
+            message: "Which license does this project fall under?",
+            name: "license",
+            choices: [
+                "MIT",
+                "ISC",
+                "EPL",
+                "Apache 2.0",
+            ]
+        }
+    ])
+}
 
-inquirer
-	.prompt([
-		{
-			type: "input",
-			name: "name",
-			message: "What is your name?",
-		},
-		{
-			type: "input",
-			name: "location",
-			message: "Where are you from?",
-		},
-		{
-			type: "input",
-			name: "hobby",
-			message: "What is your favorite hobby?",
-		},
-		{
-			type: "input",
-			name: "food",
-			message: "What is your favorite food?",
-		},
-		{
-			type: "input",
-			name: "github",
-			message: "Enter your GitHub Username",
-		},
-		{
-			type: "input",
-			name: "linkedin",
-			message: "Enter your LinkedIn URL.",
-		},
-	])
+// const questions = [
+
+// 	title,
+// 	description,
+// 	install,
+// 	usage,
+// 	credits,
+// 	license,
+// 	badges,
+// 	features,
+// 	contributor,
+// 	tests,
+// ];
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-	fs.appendFile(`${fileName}.md`, data, (err) =>
-		err ? console.error(err) : console.log(`${filename}.md generated.`)
-	);
-}
+function writeToFile = (answers) => 
+`# ${answers.title}
+
+${answers.description}
+
+## Table of Contents
+- <a href="#inst">Install<a>
+- <a href="#use">Usage<a>
+- <a href="#credits">Credits<a>
+- <a href="#lic">License<a>
+- <a href="#test">Tests<a>
+- <a href="#questions">Questions<a>
+
+## <h1 id="inst">Install<h1>
+    
+- ${answers.install}
+
+## <h1 id="use">Usage<h1>
+    
+- ${answers.usage}
+
+## <h1 id="collab">Credits<h1>
+- ${answers.credits}
+
+- This application is covered under the ${answers.license} license
+[![License: ${answers.license}]${generateLicense(answers.license)}
+
+### <h1 id="git">GitHub Profile<h1>
+- https://github.com/${answers.github}
+
+`
+
+// {
+// 	fs.appendFile(`${fileName}.md`, data, (err) =>
+// 		err ? console.error(err) : console.log(`${filename}.md generated.`)
+// 	);
+// }
 
 // TODO: Create a function to initialize app
 function init() {}
